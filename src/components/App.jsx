e893ecdada6a826ca00eee41c37d0ee016039b60
Serhii Tarasenko <stdev33@gmail.com>
2024-02-06 23:31:53 +0200

    step 2. add possibility to input contact's phone number

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,44 +5,69 @@ class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
-  handleNameChange = event => {
-    this.setState({ name: event.target.value });
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    const { name, contacts } = this.state;
-    const newContact = { id: nanoid(), name: name };
+    const { name, number, contacts } = this.state;
+    const newContact = { id: nanoid(), name, number };
     this.setState({
       contacts: [...contacts, newContact],
       name: '',
+      number: '',
     });
   };
 
+  nameInputId = nanoid();
+  numberInputId = nanoid();
+
   render() {
-    const { contacts, name } = this.state;
+    const { contacts, name, number } = this.state;
 
     return (
       <div>
         <h1>Phonebook</h1>
         <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            value={name}
-            onChange={this.handleNameChange}
-            required
-          />
+          <label htmlFor={this.nameInputId}>
+            Name
+            <input
+              type="text"
+              name="name"
+              id={this.nameInputId}
+              pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              value={name}
+              onChange={this.handleInputChange}
+              required
+            />
+          </label>
+          <label htmlFor={this.numberInputId}>
+            Number
+            <input
+              type="tel"
+              name="number"
+              id={this.numberInputId}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              value={number}
+              onChange={this.handleInputChange}
+              required
+            />
+          </label>
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
         <ul>
           {contacts.map(contact => (
-            <li key={contact.id}>{contact.name}</li>
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
           ))}
         </ul>
       </div>
